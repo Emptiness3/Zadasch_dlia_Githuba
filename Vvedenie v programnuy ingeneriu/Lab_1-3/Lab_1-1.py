e01e767bff6dce560d90f2a82167e8715a996795
Emptiness3 <82176305+Emptiness3@users.noreply.github.com>
2021-10-17 13:42:35 +0500

    Create Lab_1-1.py

---
+++ Vvedenie v programnuy ingeneriu/Lab_1-3/Lab_1-1.py
@@ -0,0 +1,12 @@
+def Lab_1_1():
+  Isx_Str = 'xxxabcdxxx'
+  New_Str = ''
+  print(Isx_Str)
+  for i in Isx_Str:
+    if i == 'x':
+      New_Str += 'y'
+    else:
+      New_Str += i
+  print(New_Str)
+
+Lab_1_1()
