d3e5ff36589bf2482fa1ce101f0dfc75e6a2f6a9
Emptiness3 <82176305+Emptiness3@users.noreply.github.com>
2021-10-17 19:40:48 +0500

    lab

---
+++ Vvedenie v programnuy ingeneriu/Lab_1-3/Lab_0/Lab_0_2.py
@@ -0,0 +1,7 @@
+def Lab_0_2 ():
+  i = 0
+  x = int(0)
+  for i in range(88888889):
+    x += i
+  print (x)
+Lab_0_2()
---
+++ Vvedenie v programnuy ingeneriu/Lab_1-3/Lab_0/Lab_0_3.py
@@ -0,0 +1,10 @@
+def Lab_0_3():
+  x = [3, 4, 56, 100, 2, 2, 3]
+  summa = 0
+  dlina = 0
+  for i in x[:]:
+    summa += i
+  for i in x[:]:
+    dlina += 1
+  print(summa / dlina)
+Lab_0_3()
