11f845fdc1bba4f8c520c58c511844e0fc80cdbc
Emptiness3 <82176305+Emptiness3@users.noreply.github.com>
2021-10-31 11:28:15 +0500

    Lab

---
+++ Invoramatika i osnovi programmirovania/Lab_3/Zadacha_1.py
@@ -0,0 +1,25 @@
+def Zadacha_1():
+	# Input
+	s = 'Аргентина +++----манит ====:;*****"""негра.,!!!????'
+	spez= '.,!?-+*= :;"'
+	spez_2= "'"
+	otvet= []
+
+	# Base
+
+	for i in range(len(s)):
+		if s[i] not in spez:
+			if s[i] not in spez_2:
+				otvet.append(s[i])
+	line = ''.join(otvet)
+	line = line.lower()	
+	elline = line[::-1]
+	print(elline);
+	print(line);
+
+	if line == elline:
+		print("Текст являеттся палиндромом")
+	else:
+		print("Текст не является палиндромом")
+	import Startovoia;
+	Startovoia.Start();
