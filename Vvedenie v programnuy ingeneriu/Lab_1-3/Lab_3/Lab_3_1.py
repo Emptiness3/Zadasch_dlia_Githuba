5b8d2e51597f7dc3f8b4679eb7a65ab51b763d18
Emptiness3 <82176305+Emptiness3@users.noreply.github.com>
2021-11-17 01:42:46 +0500

    a

--- Vvedenie v programnuy ingeneriu/Lab_1-3/Lab_3/Lab_3_1.py
+++ Vvedenie v programnuy ingeneriu/Lab_1-3/Lab_3/Lab_3_1.py
@@ -1,27 +1,20 @@
-CharactersList = [
-  ['', '', '', '', ''],
-  [2, 1, 0, 3, 2],
-  [2, 0, 0, 1, 3],
-  [1, 2, 1, 2, 1],
-  [2, 0, 3, 1, 2]]
-def FindBestChList():
-  print('Обработка списка')
-  max = -1
-  count = 0
-  for index1 in range(len(CharactersList[0])):
-    for index2 in range(len(CharactersList[index1])):
-      if CharactersList[index2][index1] > max:
-        max = CharactersList[index2][index1]
-        count = index2
-    print('Лучший персонаж по критерию %s:' % index1, count)
-    max = -1
-    count = 0
-print('''Введите по очереди критерии условного
-персонажа, который описан списком.
-Для описания критериев используйте следующие символы:
-"0", "1", "2", "3"''')
-for index1 in range(len(CharactersList[0])):
-  CharactersList[0][index1] = int(input('Критерий %s: ' % index1))
+Tanks = [
+  ["IS-3", 2250],
+  ["Maus", 3000 ],
+  ["T110E4", 2000],
+  ["FV217 Badger", 2100],
+  ]
+
+max_hp = 0
+current_hp = 0
+
+for i in range(len(Tanks)):
+  for i2 in range(len(Tanks[i])):
+    current_hp = Tanks[i][i2+1]
+    if current_hp >= max_hp:
+      max_hp = current_hp
+    break
+print("Максимальный запрос прочности из танков равен: ") 
+print(max_hp)
 
 
-FindBestChList()
