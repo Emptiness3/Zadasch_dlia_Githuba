020bb92222f3aca3ec7a7df6b279e50a6a515636
Emptiness3 <82176305+Emptiness3@users.noreply.github.com>
2021-11-27 12:58:47 +0500

    s

---
+++ Vvedenie v programnuy ingeneriu/Lab_9-10/main.py
@@ -0,0 +1,13 @@
+"""
+    Гадалка
+"""
+from brain import Brain
+ 
+brain = Brain()
+prompt = "Что Вас интересует?"
+ 
+question = ""
+while question != "хватит": 
+    print(prompt, end = ' ')
+    answer = brain.think(input())
+    print(answer)
